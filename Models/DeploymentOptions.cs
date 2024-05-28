406c960f572e0a61a112f4101e559c1ee3a6a145
Alexander Chaker <alexander.chaker@onesky.com>
2024-05-28 15:43:52 +0300

    - Add Sortable List - Add WorkingDirectory property for better logging

--- Models/DeploymentOptions.cs
+++ Models/DeploymentOptions.cs
@@ -2,6 +2,7 @@
 
 public record class DeploymentOptions
 {
+    public string? WorkingDirectory { get; set; }
     public List<string>? FileList { get; set; }
     public string? ServerUrl { get; set; }
     public string? DBAuthenticationUser { get; set; } //user that will be user to authenticate. Could be current user if "Integrated Security"
