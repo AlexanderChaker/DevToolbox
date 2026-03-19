3675bb6b62c79538f733c11a8fd8f48dc88f2a08
Alexander Chaker <alexander.chaker@onesky.com>
2026-03-19 14:39:43 -0400

    Implement system tray functionality with icon and window management

---
+++ Platforms/Windows/TrayService.cs
@@ -0,0 +1,85 @@
+using H.NotifyIcon;
+using Microsoft.Win32;
+using WinUIControls = Microsoft.UI.Xaml.Controls;
+
+namespace DevToolbox.Platforms.Windows;
+
+public static partial class TrayService
+{
+    private static TaskbarIcon? _trayIcon;
+    private static Microsoft.UI.Xaml.Window? _window;
+
+    public static bool IsExiting { get; private set; }
+
+    public static void Initialize(Microsoft.UI.Xaml.Window window)
+    {
+        _window = window;
+
+        var iconFile = IsDarkTheme()
+            ? @"Resources\AppIcon\wrenchtool(white).ico"
+            : @"Resources\AppIcon\wrenchtool.ico";
+
+        _trayIcon = new TaskbarIcon
+        {
+            ToolTipText = "Dev Toolbox",
+            Icon = new System.Drawing.Icon(Path.Combine(AppContext.BaseDirectory, iconFile)),
+            NoLeftClickDelay = true,
+            // Left-click restores the window
+            LeftClickCommand = new SimpleCommand(RestoreWindow),
+            // RightClickCommand = new SimpleCommand(ExitApplication)
+        };
+
+        // Right-click context menu
+        // var deserializeKafkaMsg = new WinUIControls.MenuFlyoutItem { Text = "Deserialize Kafka Message", Command = new SimpleCommand(() => { }) }; // TODO
+        var showItem = new WinUIControls.MenuFlyoutItem { Text = "Show", Command = new SimpleCommand(RestoreWindow) };
+        var exitItem = new WinUIControls.MenuFlyoutItem { Text = "Exit", Command = new SimpleCommand(ExitApplication) };
+
+        var menu = new WinUIControls.MenuFlyout();
+        // menu.Items.Add(deserializeKafkaMsg); // TODO
+        menu.Items.Add(showItem);
+        menu.Items.Add(new WinUIControls.MenuFlyoutSeparator());
+        menu.Items.Add(exitItem);
+
+        _trayIcon.ContextFlyout = menu;
+
+        _trayIcon.ForceCreate();
+    }
+
+    public static void RestoreWindow()
+    {
+        if (_window == null) return;
+
+        _window.Show();
+        _window.Activate();
+    }
+
+    public static void HideWindow()
+    {
+        _window?.Hide();
+    }
+
+    public static void ExitApplication()
+    {
+        IsExiting = true;
+        _trayIcon?.Dispose();
+        _trayIcon = null;
+        _window?.Close();
+    }
+
+    private static bool IsDarkTheme()
+    {
+        using var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+        var value = key?.GetValue("AppsUseLightTheme");
+        return value is int i && i == 0;
+    }
+
+    private partial class SimpleCommand(Action execute) : System.Windows.Input.ICommand
+    {
+        private readonly Action _execute = execute;
+        public bool CanExecute(object? parameter) => true;
+        public void Execute(object? parameter) => _execute();
+#pragma warning disable CS0067
+        public event EventHandler? CanExecuteChanged;
+#pragma warning restore CS0067
+    }
+}
