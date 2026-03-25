4e9cdcf1e982aa7a4ef02300df9fca8de4b2fde5
Alexander Chaker <alexander.chaker@onesky.com>
2026-03-25 15:44:11 -0400

    Add App Version to the NavMenu

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -12,20 +12,20 @@ namespace DevToolbox;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		// Set the window title and configure tray icon
-		builder.ConfigureLifecycleEvents(events =>
+        // Set the window title and configure tray icon
+        builder.ConfigureLifecycleEvents(events =>
         {
-			#if WINDOWS
+            #if WINDOWS
             events.AddWindows(windows =>
             {
                 windows.OnWindowCreated((window) =>
@@ -50,33 +50,33 @@ public static class MauiProgram
                     };
                 });
             });
-			#endif
+            #endif
         });
 
-		builder.UseMauiCommunityToolkit(); //Used for the FolderPicker;
-		builder.Services.AddMauiBlazorWebView();
-		builder.Services.AddMudServices(config =>
-		{
-			config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
-			config.SnackbarConfiguration.PreventDuplicates = true;
-			config.SnackbarConfiguration.NewestOnTop = true;
-			config.SnackbarConfiguration.VisibleStateDuration = 3500;
-			config.SnackbarConfiguration.ShowTransitionDuration = 150;
-			config.SnackbarConfiguration.HideTransitionDuration = 150;
-			config.SnackbarConfiguration.MaxDisplayedSnackbars = 4;
-		});
-		builder.Services.AddSingleton<InMemoryLoggerService>();
+        builder.UseMauiCommunityToolkit(); //Used for the FolderPicker;
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMudServices(config =>
+        {
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+            config.SnackbarConfiguration.PreventDuplicates = true;
+            config.SnackbarConfiguration.NewestOnTop = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 3500;
+            config.SnackbarConfiguration.ShowTransitionDuration = 150;
+            config.SnackbarConfiguration.HideTransitionDuration = 150;
+            config.SnackbarConfiguration.MaxDisplayedSnackbars = 4;
+        });
+        builder.Services.AddSingleton<InMemoryLoggerService>();
 
-		#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
-		#endif
+        #if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+        #endif
 
-		builder.Services.AddSingleton<IGitService, GitService>();
-		builder.Services.AddScoped<ISQLService, SQLService>(); //Needs to be scoped since each session can set a different connection string
-		builder.Services.AddSingleton<IKafkaSerializerService, KafkaSerializerService>();
-		builder.Services.AddSingleton<TrayCommandService>();
+        builder.Services.AddSingleton<IGitService, GitService>();
+        builder.Services.AddScoped<ISQLService, SQLService>(); //Needs to be scoped since each session can set a different connection string
+        builder.Services.AddSingleton<IKafkaSerializerService, KafkaSerializerService>();
+        builder.Services.AddSingleton<TrayCommandService>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
